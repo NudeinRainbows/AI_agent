2d628c0dbb7739b859d16991e17b8c3c5e99e660
Alex Cioca <alex.s.cioca@gmail.com>
2026-06-18 20:40:16 -0400

    hardcoded

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("API key not found")
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from ai-agent!")
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model = "gemini-2.5-flash"
+    response = client.models.generate_content(model=model, contents=prompt)
+
+
+    if response.usage_metadata is None:
+        raise RuntimeError("usage_metadata is None")
+
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.total_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
